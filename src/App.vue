23e5dd1cab5a3d751d29832f513d0c838fdb9701
eidng8 <cheung.jackey@gmail.com>
2020-03-27 20:58:54 +0800

    filling up

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
+<!--
+  - GPLv3 https://www.gnu.org/licenses/gpl-3.0.en.html
+  -
+  - Author: eidng8
+  -->
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <g8-tree-view :item="item"></g8-tree-view>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
+import {Component, Vue} from 'vue-property-decorator';
+import G8TreeView from '@/components/G8TreeView.vue';
+import {G8TreeItem} from '@/components/types';
 
 @Component({
   components: {
-    HelloWorld
-  }
+    G8TreeView,
+  },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  item: G8TreeItem = {
+    key: 'item 1',
+    name: 'item 1',
+  };
+}
 </script>
 
 <style lang="scss">
