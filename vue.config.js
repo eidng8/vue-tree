4f0e55f7488ee328aa44aa929ceb1afb55fc9b6d
eidng8 <cheung.jackey@gmail.com>
2020-04-14 23:02:01 +0800

    trying to add code climate and bundle analyzer

--- vue.config.js
+++ vue.config.js
@@ -4,10 +4,17 @@
  * Author: eidng8
  */
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const BundleAnalyzerPlugin = require('@bundle-analyzer/webpack-plugin');
+
 module.exports = {
   configureWebpack: config => {
     if ('production' == process.env.NODE_ENV) {
       config.devtool = undefined;
+    }
+    if (process.env.BUNDLE_ANALYZER_TOKEN) {
+      config.plugins.push(new BundleAnalyzerPlugin(
+        {token: process.env.BUNDLE_ANALYZER_TOKEN}));
     }
   },
 };
