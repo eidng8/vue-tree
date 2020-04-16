2df415fe30dc7a895d080e95da32f3df33b058b0
eidng8 <cheung.jackey@gmail.com>
2020-04-16 22:40:12 +0800

    add webpack bundle analyzer

--- vue.config.js
+++ vue.config.js
@@ -6,15 +6,18 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const BundleAnalyzerPlugin = require('@bundle-analyzer/webpack-plugin');
+const WBA = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   configureWebpack: config => {
+    config.plugins.push(new WBA({ analyzerMode: 'static' }));
     if ('production' == process.env.NODE_ENV) {
       config.devtool = undefined;
     }
     if (process.env.BUNDLE_ANALYZER_TOKEN) {
-      config.plugins.push(new BundleAnalyzerPlugin(
-        {token: process.env.BUNDLE_ANALYZER_TOKEN}));
+      config.plugins.push(
+        new BundleAnalyzerPlugin({ token: process.env.BUNDLE_ANALYZER_TOKEN }),
+      );
     }
   },
 };
