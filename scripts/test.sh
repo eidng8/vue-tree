e621534cef5a244092a73bda5a954cca53538144
eidng8 <cheung.jackey@gmail.com>
2020-04-22 12:45:59 +0800

    report only linux to analytics (#23)

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,17 +1,46 @@
 #!/usr/bin/env bash
 
 # setup code climate
-curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
-chmod +x ./cc-test-reporter
-./cc-test-reporter before-build
+setup_cc() {
+  # use only linux for analysis
+  [[ "${TRAVIS_OS_NAME}" != 'linux' ]] && return 0
+  curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+  chmod +x ./cc-test-reporter
+  ./cc-test-reporter before-build
+}
+
+# submit to code climate
+submit_cc() {
+  # use only linux for analysis
+  [[ "${TRAVIS_OS_NAME}" != 'linux' ]] && return 0
+  ./cc-test-reporter after-build --exit-code "${RES}"
+  # cc-test-reporter may report error, just ignore it
+  return 0
+}
 
 # setup coveralls
-npm install --no-save coveralls
+setup_ca() {
+  # use only linux for analysis
+  [[ "${TRAVIS_OS_NAME}" != 'linux' ]] && return 0
+  npm install --no-save coveralls
+}
 
-# test
-npm run test:unit -- --coverage || exit 1
-cat ./coverage/lcov.info | coveralls
-npm run test:e2e || exit 1
-./cc-test-reporter after-build --exit-code "${TRAVIS_TEST_RESULT}"
-# cc-test-reporter may report error, just ignore it
-exit 0
+# submit to coveralls
+submit_ca() {
+  # use only linux for analysis
+  [[ "${TRAVIS_OS_NAME}" != 'linux' ]] && return 0
+  cat ./coverage/lcov.info | coveralls
+}
+
+RES=0
+# unit test
+setup_ca
+setup_cc
+npm run test:unit -- --coverage
+RES=$?
+submit_cc
+submit_ca
+[[ ${RES} != '0' ]] && exit 1
+
+# e2e test
+npm run test:e2e
