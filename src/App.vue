75dbd6b1d6210e3242393c3ff6685ddf4545ea33
eidng8 <cheung.jackey@gmail.com>
2020-04-30 10:53:45 +0800

    fix #48

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
     </div>
     <div>
       <button id="populate" @click="populate()">populate tree</button>
-      <ul class="g8-tree__view g8-tree__dark g8-tree__highlight_hover">
+      <ul class="g8-tree__view g8-tree__dark g8-tree--highlight-hover">
         <g8-tree-view
           item-id="key"
           item-label="text"
