ad78c40ee506d0bf0f8e724d81830e2ba1e83570
eidng8 <cheung.jackey@gmail.com>
2020-04-30 15:42:53 +0800

    fix #52

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@
  * Author: eidng8
  */
 
-export { G8TreeItem, G8TreeItemTag } from './components/types';
+export { G8TreeItem, G8TreeItemTag, G8ClickEvent } from './components/types';
 export { default as G8VueTree } from './components/G8VueTree.vue';
